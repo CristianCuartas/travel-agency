f67c583acde763c979a4bb1a0d9f1611c5d1e254
cristian <cristianhz1109@gmail.com>
2019-11-25 15:52:58 -0500

    Deployment to heroku

---
+++ server/models/Viajes.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+const db = require('../config/database');
+
+const Viaje = db.define('viajes', {
+  titulo: {
+    type: Sequelize.STRING
+  },
+  precio: {
+    type: Sequelize.STRING
+  },
+  fecha_ida: {
+    type: Sequelize.DATE
+  },
+  fecha_vuelta: {
+    type: Sequelize.DATE
+  },
+  imagen: {
+    type: Sequelize.STRING
+  },
+  descripción: {
+    type: Sequelize.STRING
+  },
+  disponibles: {
+    type: Sequelize.STRING
+  }
+});
+module.exports = Viaje;
